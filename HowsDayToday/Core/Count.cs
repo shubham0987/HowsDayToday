f820905f09965430db5159199e4d943c5b471dc5
Shubham Saxena <shubham0987@gmail.com>
2013-07-17 16:53:39 +0530

    all files added
    first commit

---
+++ HowsDayToday/Core/Count.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace HowsDayToday.Core
+{
+    public class Count
+    {
+        public string name;
+        public DateTime birthday;
+
+        public int Tday, Tmonth, Tyear;
+
+        public int BDay, BMonth, BYear;
+
+        public int nameScore;
+
+        public Count(string name,DateTime birthday)
+        {
+            name=name.ToLower();
+            this.name = name;
+            this.birthday = birthday;
+
+            DateTime today = DateTime.Now;
+
+            Tday = today.Day;
+            Tmonth = today.Month;
+            Tyear = today.Year;
+
+            BDay = birthday.Day;
+            BMonth = birthday.Month;
+            BYear = birthday.Year;
+
+            nameScore = 0;
+            
+            for (int i = 0; i < name.Length; i++)
+            {
+                nameScore += name[i];
+            }
+        }
+
+
+        public int CalculateSocial()
+        {
+            long value = 0;
+
+            value += nameScore + Tday + BDay + Tmonth + BMonth * BYear + Tday + Tmonth * Tyear;
+
+            return (int)value%100;
+        }
+
+        public int CalculateRelationships()
+        {
+            long value = 0;
+            value += nameScore + Tday * BDay * BMonth + BYear + Tmonth + Tday + Tmonth / Tyear;
+            return (int)value%100;
+        }
+
+        public int CalculateWork()
+        {
+            long value = 0;
+            value += Tmonth + Tday * BMonth * Tday + Tmonth + Tyear*Tday;
+            return (int)value%100;
+        }
+
+        public int CalculatePersonal()
+        {
+            long value = 0;
+            value += Tday * BMonth + Tday * BYear + nameScore * Tday + Tmonth + Tyear + Tday * Tmonth + Tyear;
+            return (int)value%100;
+        }
+
+        public int CalculateFinance()
+        {
+            long value = 0;
+            value += nameScore * Tmonth + nameScore * Tday + nameScore * Tyear+Tday+Tmonth*Tyear;
+            return (int)value%100;
+        }
+
+        public int CalculateEntertainment()
+        {
+            long value = 0;
+            value += nameScore * BMonth + nameScore * Tday + nameScore * BYear + Tday + Tmonth * Tyear;
+            return (int)value%100;
+        }
+
+        
+
+
+    }
+}
---
+++ HowsDayToday/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace HowsDayToday
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                if (name.Text.Length > 0 && name.Text.Length <= 50)
+                {
+                    DateTime res;
+                    if (DateTime.TryParse(birthdayTB.Text, out res))
+                    {
+                        Core.Count obj = new Core.Count(name.Text.Trim(), DateTime.Parse(birthdayTB.Text.Trim()));
+                        SocialPB.Value = obj.CalculateSocial();
+                        RelationshipPB.Value = obj.CalculateRelationships();
+                        WorkPB.Value = obj.CalculateWork();
+                        PersonalPB.Value = obj.CalculatePersonal();
+                        financePB.Value = obj.CalculateFinance();
+                        EntertainmentPB.Value = obj.CalculateEntertainment();
+
+                        Appear.Begin();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Enter your correct birthday in the form MM/DD/YYYY.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Name should be between 1 and 50 characters");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Oops something went wrong, Please try again");
+            }
+            
+
+        }
+
+       
+
+        
+    }
+}
